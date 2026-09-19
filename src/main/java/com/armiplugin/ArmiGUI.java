58897e789a69f330a6b31e222c2a141e774abe1b
Assonnatooh <annoixtooh@gmail.com>
2026-09-19 12:33:31 +0200

    Add Beretta 92FS slots to ArmiGUI

--- src/main/java/com/armiplugin/ArmiGUI.java
+++ src/main/java/com/armiplugin/ArmiGUI.java
@@ -4,24 +4,32 @@ import org.bukkit.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 
 public class ArmiGUI {
 
     // Titolo usato anche per riconoscere l'inventario nel listener dei click
     public static final String TITLE = ChatColor.DARK_GRAY + "Armeria";
 
-    // Slot fissi dentro la GUI
+    // Slot Glock (Prima riga)
     public static final int SLOT_GLOCK = 10;
     public static final int SLOT_CARICATORE = 12;
     public static final int SLOT_MUNIZIONI = 14;
 
+    // Slot Beretta 92FS (Seconda riga)
+    public static final int SLOT_BERETTA = 19;
+    public static final int SLOT_CARICATORE_BERETTA = 21;
+
     public static void open(Player player) {
         Inventory gui = Bukkit.createInventory(null, 27, TITLE);
 
+        // Glock
         gui.setItem(SLOT_GLOCK, ItemFactory.createGlock());
         gui.setItem(SLOT_CARICATORE, ItemFactory.createCaricatoreGlock(0));
         gui.setItem(SLOT_MUNIZIONI, ItemFactory.createMunizioni9mm(64));
+
+        // Beretta 92FS
+        gui.setItem(SLOT_BERETTA, ItemFactory.createBeretta92FS());
+        gui.setItem(SLOT_CARICATORE_BERETTA, ItemFactory.createCaricatoreBeretta(0));
 
         player.openInventory(gui);
     }
